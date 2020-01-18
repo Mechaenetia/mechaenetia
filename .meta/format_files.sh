4269ff1c3b06ab4a29d9d81dee5c77b416ce505c
Gregorius Techneticies <gregoriustech@gmail.com>
2020-01-19 00:09:10 +0100

    Renamed some scripts to fit better, naming scheme wise

--- .meta/format_files.sh
+++ .meta/format_files.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-echo "Formatting all Source Files"
+echo "Formatting all Files"
 
 SOURCE_DIR=$(dirname "$0")
 
 find "$SOURCE_DIR/../assets" -iname "*.png" -mtime -30 -exec optipng -o7 -clobber -silent {} \;
 
-echo "Done formatting all Source Files"
+echo "Done formatting all Files"
