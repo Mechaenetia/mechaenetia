39bee8316fbe14c99ff8a83df1cb8da6c2139548
OvermindDL1 <overminddl1@gmail.com>
2021-03-30 14:01:44 -0600

    Setup default plugins of bevy for now, empty window now displays that can be closed.

--- src/experimenting.rs
+++ src/experimenting.rs
@@ -3,6 +3,9 @@
 use bevy::prelude::*;
 
 pub fn start() -> anyhow::Result<()> {
-	App::build().run();
+	App::build()
+		// DefaultPlugins sets up the standard renderer, winit, logger, some basic components, etc... depending on features enabled
+		.add_plugins(DefaultPlugins)
+		.run();
 	Ok(())
 }
