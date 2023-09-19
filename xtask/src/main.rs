9b921614cea99a98c4b36000034354b2eaf50fb1
OvermindDL1 <overminddl1@gmail.com>
2023-09-19 11:13:03 -0600

    Remove the old license check functionality from xtask since it has now been replaced by cargo-deny

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,7 +1,6 @@
 #![warn(clippy::pedantic)]
 
 use semver::{Version, VersionReq};
-use std::collections::HashMap;
 use std::io::IsTerminal;
 use std::sync::OnceLock;
 use xtask::not_bash::run;
@@ -61,7 +60,6 @@ SUBCOMMANDS:
 fn cmd_checks(_args: &mut std::env::Args) {
 	eprintln!("Running checks...");
 	cmd_checks_cargo_deny();
-	cmd_checks_licenses();
 }
 
 fn cmd_checks_cargo_deny() {
@@ -101,86 +99,4 @@ fn cmd_checks_cargo_deny() {
 			eprintln!("cargo-deny failed, please correct prior errors");
 		}
 	}
-}
-
-fn cmd_checks_licenses() {
-	#[derive(serde::Deserialize)]
-	struct Meta<'s> {
-		#[serde(borrow)]
-		packages: Vec<MetaPackage<'s>>,
-	}
-	#[derive(serde::Deserialize)]
-	struct MetaPackage<'s> {
-		#[serde(borrow)]
-		name: &'s str,
-		license: Option<&'s str>,
-	}
-	print!("> Licenses...");
-	let expected = [
-		"(MIT OR Apache-2.0) AND Unicode-DFS-2016",
-		"0BSD OR MIT OR Apache-2.0",
-		"Apache-2.0",
-		"Apache-2.0 / MIT",
-		"Apache-2.0 OR BSL-1.0",
-		"Apache-2.0 OR MIT",
-		"Apache-2.0 WITH LLVM-exception OR Apache-2.0 OR MIT",
-		"Apache-2.0/MIT",
-		"BSD-2-Clause",
-		"BSD-2-Clause OR MIT OR Apache-2.0",
-		"BSD-3-Clause",
-		"BSD-3-Clause OR MIT OR Apache-2.0",
-		"CC0-1.0",
-		"CC0-1.0 OR Artistic-2.0",
-		// Do NOT add GPL to the general list, it's only allowed for this project itself
-		// "GPL-3.0-or-later",
-		"ISC",
-		"MIT",
-		"MIT / Apache-2.0",
-		"MIT OR Apache-2.0",
-		"MIT OR Apache-2.0 OR Zlib",
-		"MIT OR Zlib OR Apache-2.0",
-		"MIT-0",
-		"MIT/Apache-2.0",
-		"Unlicense OR MIT",
-		"Unlicense/MIT",
-		"Zlib",
-		"Zlib AND (MIT OR Apache-2.0)",
-		"Zlib OR Apache-2.0 OR MIT",
-	];
-	// Only GPL we want to accept is within this project itself
-	let special_cased = [
-		("xtask", "GPL-3.0-or-later"),
-		("mechaenetia_client", "GPL-3.0-or-later"),
-		("mechaenetia_client_wgpu", "GPL-3.0-or-later"),
-		("mechaenetia_engine", "GPL-3.0-or-later"),
-		("mechaenetia_server", "GPL-3.0-or-later"),
-		("mechaenetia_server_dedicated", "GPL-3.0-or-later"),
-		("mechaenetia_utils", "GPL-3.0-or-later"),
-	]
-	.into_iter()
-	.collect::<HashMap<&'static str, &'static str>>();
-
-	let meta = run!("cargo metadata --format-version 1"; echo = false).unwrap();
-	let meta: Meta = serde_json::from_str(&meta).unwrap();
-
-	let mut errors = String::new();
-	for MetaPackage { name, license } in meta.packages {
-		if let Some(license) = license {
-			let license = license.trim();
-			if let Some(sc) = special_cased.get(name) {
-				if *sc == license {
-					continue;
-				}
-				errors.push_str(&format!("Special cased license for {name}: {license} != {sc}\n"));
-			}
-			if !expected.contains(&license) {
-				errors.push_str(&format!("Unknown license for {name}: {license}\n"));
-			}
-		} else {
-			errors.push_str(&format!("No license for {name}\n"));
-		}
-	}
-
-	assert!(errors.is_empty(), "Errors:\n{errors}");
-	println!("Passed");
 }
